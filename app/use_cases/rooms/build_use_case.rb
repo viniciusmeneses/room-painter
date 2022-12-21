8e2d487042ccf1bd31e80088f9b715cf66f95c8c
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-21 15:08:25 -0300

    refactor: simplify use cases

--- app/use_cases/rooms/build_use_case.rb
+++ app/use_cases/rooms/build_use_case.rb
@@ -1,7 +1,5 @@
 module Rooms
-  class BuildUseCase < Micro::Case
-    attributes :walls
-
+  class BuildUseCase < ApplicationUseCase
     Schema = Dry::Schema.Params do
       required(:walls).array(:hash) do
         required(:width).filled(:decimal)
@@ -11,40 +9,50 @@ module Rooms
       end
     end
 
-    BuildSurface = ->(width:, height:) { Room::Surface.new(width: width.to_d, height: height.to_d) }
+    class InvalidRoom < StandardError
+      attr_reader :messages
+      def initialize(messages) = @messages = messages
+    end
 
-    def call!
-      schema = Schema[attributes]
-      return Failure(:invalid_attributes, result: schema.errors.to_h) if schema.failure?
+    attribute :walls
 
-      room = build_room(**schema.to_h)
-      validate_room(room)
+    def execute
+      room = build_room
+      validate_room!(room)
+      validate_walls!(room.walls)
+      Success(result: { room: })
+    rescue InvalidRoom => error
+      Failure(:invalid_room, result: error.messages)
     end
 
     private
 
-    def build_room(walls:)
+    def build_room
       room_walls = walls.map do |wall|
         Room::Wall.new(
           **wall,
-          doors: Array.new(wall[:doors]) { BuildSurface[width: 0.8, height: 1.9] },
-          windows: Array.new(wall[:windows]) { BuildSurface[width: 2.0, height: 1.2] }
+          doors: Array.new(wall[:doors]) { build_surface(width: 0.8, height: 1.9) },
+          windows: Array.new(wall[:windows]) { build_surface(width: 2.0, height: 1.2) }
         )
       end
 
       Room.new(walls: room_walls)
     end
 
-    def validate_room(room)
-      return Failure(:invalid_room, result: room.errors.messages) if room.invalid?
+    def build_surface(width:, height:)
+      Room::Surface.new(width: width.to_d, height: height.to_d)
+    end
 
-      if room.walls.map(&:invalid?).any?
-        messages = room.walls.map { |wall| wall.errors.messages }
-        messages_by_index = messages.index_by.with_index { |_, index| index }.select { |_, messages| messages.any? }
-        return Failure(:invalid_walls, result: { walls: messages_by_index })
-      end
+    def validate_room!(room)
+      raise InvalidRoom, room.errors.messages if room.invalid?
+    end
 
-      Success(result: { room: })
+    def validate_walls!(walls)
+      return if walls.map(&:valid?).any?
+
+      messages = walls.map { |wall| wall.errors.messages }
+      messages_by_index = messages.index_by.with_index { |_, index| index }.select { |_, messages| messages.any? }
+      raise InvalidRoom, walls: messages_by_index
     end
   end
 end
--- app/use_cases/rooms/paint_use_case.rb
+++ app/use_cases/rooms/paint_use_case.rb
@@ -1,34 +1,59 @@
 module Rooms
-  class PaintUseCase < Micro::Case
-    attributes :room, required: true
-
-    BuildPaintCan = ->(size:, area_per_liter: 5.0) { PaintCan.new(size: size.to_d, area_per_liter: area_per_liter.to_d) }
-
-    def call!
-      remaining_area = room.paintable_area
-
-      quantity_by_paint_can = paint_cans.each.with_index.reduce({}) do |quantities, (can, index)|
-        quantity = (remaining_area / can.paint_area).floor
-        remaining_area -= can.paint_area * quantity
-
-        is_last_can = paint_cans.size - 1 == index
-        quantity += 1 if is_last_can && remaining_area.positive?
-
-        quantity.positive? ? { **quantities, can => quantity } : quantities
+  class PaintUseCase < ApplicationUseCase
+    Schema = Dry::Schema.Params do
+      required(:room).filled
+      required(:paint_cans).array(:hash) do
+        required(:size).filled(:decimal)
+        required(:area_per_liter).filled(:decimal)
       end
+    end
 
-      Success(result: { area: room.paintable_area, paint_cans: quantity_by_paint_can })
+    DEFAULT_PAINT_CANS = [
+      { size: 18, area_per_liter: 5 },
+      { size: 3.6, area_per_liter: 5 },
+      { size: 2.5, area_per_liter: 5 },
+      { size: 0.8, area_per_liter: 5 }
+    ]
+
+    attribute :room
+    attribute :paint_cans, default: DEFAULT_PAINT_CANS
+
+    def execute
+      paintable_area = room.paintable_area
+
+      needed_paint_cans = calculate_needed_cans(
+        cans: build_paint_cans,
+        paintable_area:
+      )
+
+      Success(result: { area: paintable_area, paint_cans: needed_paint_cans })
     end
 
     private
 
-    def paint_cans
-      @paint_cans ||= [
-        BuildPaintCan[size: 18.0],
-        BuildPaintCan[size: 3.6],
-        BuildPaintCan[size: 2.5],
-        BuildPaintCan[size: 0.8]
-      ]
+    def build_paint_cans
+      paint_cans.map do |can|
+        PaintCan.new(
+          size: can[:size],
+          area_per_liter: can[:area_per_liter]
+        )
+      end
     end
+
+    def calculate_needed_cans(paintable_area:, cans:)
+      remaining_area = paintable_area
+
+      cans.each.with_index.reduce({}) do |quantities, (can, index)|
+        paint_area = can.paint_area
+
+        quantity = (remaining_area / paint_area).floor
+        remaining_area -= paint_area * quantity
+        quantity += 1 if last_can?(index) && remaining_area.positive?
+
+        quantity.positive? ? { **quantities, can => quantity } : quantities
+      end
+    end
+
+    def last_can?(index) = paint_cans.size - 1 == index
   end
 end
