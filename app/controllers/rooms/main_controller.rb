90a2b2389f1527be4c9200e6afd1a1ef726dab56
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-21 11:48:53 -0300

    feat: implement paint action in rooms controller

--- app/controllers/rooms/main_controller.rb
+++ app/controllers/rooms/main_controller.rb
@@ -1,7 +1,17 @@
 module Rooms
   class MainController < ApplicationController
     def paint
-      head :ok
+      Rooms::BuildUseCase.call(**params.permit!)
+        .then(Rooms::PaintUseCase)
+        .on_success do |result|
+          render(json: {
+            area: result[:area].to_f,
+            paint_cans: result[:paint_cans].map do |paint_can, quantity|
+              { size: paint_can.size.to_f, quantity: }
+            end
+          })
+        end
+        .on_failure { |result| render(status: :unprocessable_entity, json: { errors: result.data }) }
     end
   end
 end
