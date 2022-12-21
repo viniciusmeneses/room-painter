252f4a0ee9a4905510782115e94c9c6010431269
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-20 23:23:39 -0300

    test: room calculate paintable area

--- spec/factories/room/wall.rb
+++ spec/factories/room/wall.rb
@@ -3,14 +3,7 @@ FactoryBot.define do
     width { 7 }
     height { 4 }
 
-    doors do
-      surface = build(:room_surface, width: 0.8, height: 1.9)
-      [surface]
-    end
-
-    windows do
-      surface = build(:room_surface, width: 1.5, height: 0.7)
-      [surface, surface]
-    end
+    doors { Array.new(1) { association(:room_surface, width: 0.8, height: 1.9) } }
+    windows { Array.new(1) { association(:room_surface, width: 1.5, height: 0.7) } }
   end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -11,9 +11,22 @@ RSpec.describe Room, type: :model do
   describe "#walls" do
     context "when size is not 4" do
       it "is invalid" do
-        paint_can = build(:room, walls: [])
-        expect(paint_can).to be_invalid
+        room = build(:room, walls: [])
+        expect(room).to be_invalid
       end
+    end
+  end
+
+  describe "#paintable_area" do
+    it "calculates area based on walls paintable area" do
+      door = build(:room_surface, width: 1, height: 2)
+      window = build(:room_surface, width: 2, height: 1)
+      wall = build(:room_wall, width: 10, height: 5, doors: [door], windows: [window])
+      room = build(:room, walls: [wall, wall])
+
+      paintable_area = room.paintable_area
+
+      expect(paintable_area).to eq(92)
     end
   end
 end
